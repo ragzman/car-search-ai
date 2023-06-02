2efd88f67ff05bf8b3d686c40fa96ebeaf198f81
Aditya Goyal <addyragz@gmail.com>
2023-06-02 11:13:06 -0500

    minor cleanup

--- main.py
+++ main.py
@@ -23,20 +23,8 @@ app = FastAPI()
 
 # #TODO: Check if cors is needed after angular is added to static.
 origins = ["http://localhost:4200"]
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
 
 sio = socketio.AsyncServer(async_mode="asgi", cors_allowed_origins=origins)
-# app = web.Application()
-# sio.attach(app)
-
-# sio = socketio.AsyncServer(async_mode="asgi", cors_allowed_origins=origins)
-
 socketio_app = socketio.ASGIApp(sio, app)
 app.mount("/", socketio_app)
 
@@ -52,49 +40,48 @@ from langchain.prompts import PromptTemplate
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
-    logging.info("*********In ReadRoot:")
+    # logging.info("*********In ReadRoot:")
     return templates.TemplateResponse("index.html", {"request": request})
 
 
 @sio.event
 def connect(sid, environ):
-    logging.info("**********Connected.")
+    # logging.info("**********Connected.")
     print("connect ", sid)
+
 
 @sio.event
 async def message(sid, data):
     print("message ", data)
-    logging.info(f"********* message received: {data} ")
+    # logging.info(f"********* message received: {data} ")
     await sio.emit("message", data, room=sid)
+
 
 @sio.event
 def disconnect(sid):
-    print('disconnect ', sid)
-    logging.info("**********Disconnected.")
+    logging.info(f"**********Disconnected. {sid}")
 
 
 # WebSocket endpoint
-@sio.on('chat')
+@sio.on("chat")
 async def chat(sid, data):
-    logging.info(f'**********Received a message in chat {data}')
+    # logging.info(f"**********Received a message in chat {data}")
     try:
-        logging.info(data)  # TODO: remove logging.
         cm = ChatMessage(**data)
     except ValidationErr as e:
         logging.error(e)
     chain: LLMChain = createChain(sid, sio)
     await chain.arun(cm.message)
-    logging.info('Chain complete. ')
+    # logging.info("Chain complete. ")
     end_resp = ChatMessage(
         sender=MessageSender.AI, message="", type=MessageType.STREAM_END
     )
-    await sio.emit('chat', end_resp.toJson(), room=sid)
-    logging.info('Done. ')
-
+    await sio.emit("chat", end_resp.toJson(), room=sid)
+    # logging.info("Done. ")
 
 
 def createChain(sid, sio):
-    """Create the langhcain chain. """
+    """Create the langhcain chain."""
     stream_handler = StreamingLLMCallbackHandler(sid, sio)
     llm = OpenAI(
         temperature=0.9, streaming=True, callbacks=[stream_handler], verbose=True
@@ -111,4 +98,3 @@ if __name__ == "__main__":
     import uvicorn
 
     uvicorn.run(app, host="0.0.0.0", port=8000)
-
