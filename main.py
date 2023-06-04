cc616657c4ca4405fcd713edf439dde9181160b1
Aditya Goyal <addyragz@gmail.com>
2023-06-03 21:34:01 -0500

    move some chain stuff to ai dir

--- main.py
+++ main.py
@@ -2,14 +2,13 @@ import logging
 from xml.dom import ValidationErr
 
 from dotenv import load_dotenv
-from fastapi import FastAPI, Request, WebSocket, WebSocketDisconnect
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from langchain.chains import LLMChain
-from langchain.llms import OpenAI
+from langchain import LLMChain
+from ai.query import createChain, generate_question, queryDocs
 
-from callback import StreamingLLMCallbackHandler
 from models.schemas import ChatMessage
 from models.schemas import MessageSender
 from models.schemas import MessageType
@@ -19,7 +18,6 @@ import pickle
 from pathlib import Path
 from typing import Optional
 from langchain.vectorstores import VectorStore
-from langchain.chat_models import ChatOpenAI
 
 vectorstore: Optional[VectorStore] = None
 
@@ -42,7 +40,6 @@ templates = Jinja2Templates(directory="templates")
 
 # TODO(Aditya): fix logging config location.
 logging.basicConfig(level=logging.INFO)
-from langchain.prompts import PromptTemplate
 
 @app.on_event("startup")
 async def startup_event():
@@ -89,8 +86,9 @@ async def chat(sid, data):
     #TODO: Add chat history
     reinterpretedQuestion = await generate_question(cm.message, "")
     # TODO: add number of docs fetched. 
-    docs = await queryDocs(reinterpretedQuestion, vectorstore, 2)
-    chain: LLMChain = await createChain(sid, sio) #TODO: fix the parameters passed. 
+    k = 2
+    docs = await queryDocs(reinterpretedQuestion, vectorstore, k)
+    chain: LLMChain = await createChain(sid, sio)
     await chain.arun({
         'question': reinterpretedQuestion, 
         'context' : docs,
@@ -108,63 +106,8 @@ async def chat(sid, data):
     await sio.emit("chat", command_msg.toJson(), room=sid)
     # logging.info("Done. ")
 
-# TODO: maybe simplify this method to take in less params
-async def createChain(sid: str, sio: socketio.AsyncServer):
-    """Create the langhcain chain."""
-    stream_handler = StreamingLLMCallbackHandler(sid, sio)
-    # fix the llm used here. 
-    llm = OpenAI(
-        temperature=0.9, streaming=True, callbacks=[stream_handler], verbose=True
-    )
-    prompt = PromptTemplate(
-        input_variables=["question", "context", "chat_history"],
-        template=QA_PROMPT,
-    )
-    chain = LLMChain(llm=llm, prompt=prompt)
-    return chain
 
-CONDENSE_PROMPT = """Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
-Chat History:
-{chat_history}
-Follow Up Input: {question}
-Standalone question:""";
-
-# TODO: check temperature. 
-chatModel = ChatOpenAI(
-  temperature= 0,
-  streaming= True,
-  verbose= True,
-)
-
-#TODO: add URLs in the prompt.
-QA_PROMPT = """You are an AI assistant for Chatables.ai. We are a company that builds AI based chatbots.
-You are provided a "Question" from our users, some related content from our website as "Context" and previous chat history as "History".
-Provide a conversational answer to the user's question based on the information in the context.
-If you don't know the answer, nudge the user for more information. Always be friendly and helpful.
-Context: {context}
-Question: {question}
-History : {chat_history}
-Limit your answer to 125 words. Always return URLs in the answer as markdown links associated to the title of the blog or name of product.
-Answer in Markdown:"""
-
-async def generate_question(user_input: str, chat_history: str) -> str:
-    if(chat_history.strip() == ""):
-        logging.info(f'Chat history is empty. not reinterpretting the question.')
-        return user_input
-    prompt = PromptTemplate(
-        template=CONDENSE_PROMPT,
-        input_variables=["question", "chat_history"]
-    )
-    chain = LLMChain(llm=chatModel, prompt=prompt)
-    res = await chain.arun(question =  user_input, chat_history= chat_history)
-    logging.info(f"Debug: ReInterpreted question: {res}")
-    return res
-
-async def queryDocs(updatedQuestion: str, vectorStore: VectorStore, k: int):
-    docs = vectorStore.similarity_search(updatedQuestion, k=k)
-    logging.info(f'similarity search returned : {docs}')
-    content = [d.page_content for d in docs]
-    return "  ".join(content) #TODO: maybe concat better. 
+#TODO: add a page for bot-first-website-design
 
 if __name__ == "__main__":
     import uvicorn
